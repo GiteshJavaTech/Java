0109e21edc5f4f036fbb94f9f3c0ec9d4f47207b
gitesh <giteshkumar99@gmail.com>
2024-12-28 21:37:08 +0530

    This is java practice fist commit

---
+++ JavaPracticle/src/main/java/com/java/practices/JavaPracticleApplication.java
@@ -0,0 +1,13 @@
+package com.java.practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracticleApplication.class, args);
+	}
+
+}
---
+++ JavaPracticle/src/test/java/com/java/practices/JavaPracticleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPracticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
